0c09c7b378d310dfe02c915a0bb4f2304ca7deeb
Peter Radko <cmtvpetya@gmail.com>
2023-09-26 13:49:57 +0300

    Added content to import blocks

---
+++ dist/src/content/index.d.ts
@@ -0,0 +1,17 @@
+import AccentBlock from "./block/accentBlock/block";
+import Array from "./block/array/block";
+import Heading from "./block/heading/block";
+import ErrorImage from "./block/errorImage/block";
+import Gallery from "./block/gallery/block";
+import Hr from "./block/hr/block";
+import Html from "./block/html/block";
+import Image from "./block/image/block";
+import Include from "./block/include/block";
+import List from "./block/list/block";
+import Math from "./block/math/block";
+import Spoiler from "./block/spoiler/block";
+import Table from "./block/table/block";
+import Task from "./block/task/block";
+import IMath from "./inliner/imath/inliner";
+import Link from "./inliner/link/inliner";
+export { AccentBlock, Array, ErrorImage, Gallery, Heading, Hr, Html, Image, Include, List, Math, Spoiler, Table, Task, IMath, Link, };
